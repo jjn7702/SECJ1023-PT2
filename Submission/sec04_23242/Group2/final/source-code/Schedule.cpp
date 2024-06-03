0bf86b9d66e9d28ad201c7fc66133cc83db6620c
Lyson Ng De Sheng <147515831+Backfire301@users.noreply.github.com>
2024-06-03 21:59:09 +0800

    Update Schedule.cpp

--- Submission/sec04_23242/Group2/final/source-code/Schedule.cpp
+++ Submission/sec04_23242/Group2/final/source-code/Schedule.cpp
@@ -3,10 +3,15 @@
 #include <iomanip>
 using namespace std;
 
-Schedule :: Schedule(bool s = true):status(s){}
+Schedule :: Schedule(bool s = true): status(s){}
+Schedule :: ~Schedule() {}
 
 void Schedule :: setStatus(bool s){status = s;}
 
 void Schedule :: printStatus(){
-    cout << "status: " << status << endl;
+    cout << "Status: ";
+    if(status)
+        cout << "Occupied\n";
+    else    
+        cout << "Free\n";
 }
