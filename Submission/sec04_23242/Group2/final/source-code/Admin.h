766e0d8638fbc2158716009f824d1addf047e57c
Nik Balqis <148343355+nikbalqis@users.noreply.github.com>
2024-06-03 22:22:51 +0800

    Add files via upload

---
+++ Submission/sec04_23242/Group2/final/source-code/Admin.h
@@ -0,0 +1,22 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include "User.h"
+#include <string>
+using namespace std;
+
+class Admin {
+    private:
+        string adminUsername;
+        string adminPassword;
+
+    public:
+        Admin();
+        Admin(string, string);
+
+        void resetPassword(User user[]);
+        void removeUser(User user[], int &);
+        void editUser(User []);
+    };
+
+#endif
