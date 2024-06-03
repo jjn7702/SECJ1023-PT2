c34c3677318a4f57a22d91a6b6a2a9b0bd830b13
Lyson Ng De Sheng <147515831+Backfire301@users.noreply.github.com>
2024-06-03 21:58:23 +0800

    Update Date.cpp

--- Submission/sec04_23242/Group2/final/source-code/Date.cpp
+++ Submission/sec04_23242/Group2/final/source-code/Date.cpp
@@ -3,7 +3,17 @@
 #include <iomanip>
 using namespace std;
 
-Date :: Date(bool s = true, int y = 0, int m = 0, int d = 0): year(y), month(m), day(d), Schedule(status){}
+Date :: Date(): Schedule(NULL), year(0), month(0), day(0) {}
+Date :: Date(bool s = false, int y = 0, int m = 0, int d = 0): year(y), month(m), day(d), Schedule(s){}
+Date :: ~Date() {}
+
+void Date :: setDay(int d) { day = d;}
+void Date :: setMonth(int m) { month = m;}
+void Date :: setYear(int y) { year = y;}
+
+int Date :: getDay() const { return day;}
+int Date :: getMonth() const { return month;}
+int Date :: getYear() const { return year;}
 
 void Date :: printDate(){
     cout << "Date: ";
