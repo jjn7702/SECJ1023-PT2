7bf9c904b2b8198e9f4bf4935710fb2c91997d89
Lyson Ng De Sheng <147515831+Backfire301@users.noreply.github.com>
2024-06-03 21:59:27 +0800

    Update Schedule.h

--- Submission/sec04_23242/Group2/final/source-code/Schedule.h
+++ Submission/sec04_23242/Group2/final/source-code/Schedule.h
@@ -6,7 +6,12 @@ class Schedule{
 
     public:
         Schedule(bool);
+        ~Schedule();
+
+        //mutators
         void setStatus(bool);
+
+        //accessors
         void printStatus();
 };
 
