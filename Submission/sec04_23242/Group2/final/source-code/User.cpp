db04934f6db2197d05ccbc946da4a245234166df
Nik Balqis <148343355+nikbalqis@users.noreply.github.com>
2024-06-03 22:19:52 +0800

    Add files via upload

---
+++ Submission/sec04_23242/Group2/final/source-code/User.cpp
@@ -0,0 +1,49 @@
+#include "User.h"
+#include"Schedule.h"
+#include<iostream>
+
+User :: User() : username(""), password(""), id(0) {}
+
+User :: User(string n, string p, Schedule s, int _id) : username(n), password(p), schedule(s), id(_id) {}
+
+void User :: newUser() {
+    
+    Schedule s(true); //assume default
+    int _id = 0; //default id
+    cout << "Username\t: " ;
+    cin >> username;
+    cout << "Password\t: ";
+    cin >> password;
+}
+
+bool User :: checkPassword(string p) {
+    if(p == password ){
+        cout << "Password matched.";        
+        return true;
+    }else
+        cout << "Password did not match. Try again.";
+        return false;
+        
+}
+string User :: getUsername() const {
+    return username;
+}
+int User :: getID() const {
+    return id;
+}
+Schedule User :: getSchedule() const {
+    return schedule;
+}
+
+void User :: setUsername(string n) {
+    username = n;
+}
+void User :: setPassword(string p) {
+    password = p;
+}
+void User :: setSchedule(Schedule s) {
+    schedule = s;
+}
+void User :: setID(int _id) {
+    id = _id;
+}
