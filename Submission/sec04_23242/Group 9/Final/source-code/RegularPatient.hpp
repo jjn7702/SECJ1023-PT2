291dbfb1e76dbd96520142c4cacc93031769bd91
Nur Arisha <147675471+ar1sha55@users.noreply.github.com>
2024-06-22 20:26:05 +0800

    Create RegularPatient.hpp

---
+++ Submission/sec04_23242/Group 9/Final/source-code/RegularPatient.hpp
@@ -0,0 +1,29 @@
+class RegularPatient : public Patient{
+    private:
+    string contactInfo, emergencyContact;
+
+    public:
+    RegularPatient(string contact=" ", string emergency=" "): 
+    contactInfo(contact), emergencyContact(emergency) {}
+ 
+    //mutators
+    void setcontactInfo(const string &cont) {contactInfo = cont;}
+    void setemergencyContact(const string &emercon) {emergencyContact = emercon;}
+
+    //accessors
+    string getcontactInfo() const{return contactInfo;}
+    string getemergencyContact() const{return emergencyContact;}
+
+    //using polymorphism
+    void getData() {
+        Patient::getData();
+        cout << "\t\tContact Info (+60): ";
+        getline(cin, contactInfo);
+        cout << "\t\tEmergency Contact (+60): ";
+        getline(cin, emergencyContact);
+    }
+
+    ~RegularPatient() {} //destructor
+};
+
+//-------------------------------------------------------
