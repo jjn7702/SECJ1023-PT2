78ffddc729d3b7e4fba1c721770df072a3f2b549
Chuah Hui Wen <chwen0912@gmail.com>
2024-06-24 23:40:04 +0800

    Chuah Hui Wen

--- Submission/sec08_23242/Bookworm/Final/source-code/code.cpp
+++ Submission/sec08_23242/Bookworm/Final/source-code/code.cpp
@@ -5,3 +5,32 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+
+class Publisher {
+	private:
+    	string publisherName;
+		string country;
+	
+	public:
+	    Publisher() {}
+	    
+	    Publisher(string n, string c) : publisherName(n), country(c) {}
+	    
+		~Publisher() {}
+		
+	    string getPublisherName() const { 
+			return publisherName; 
+		}
+		
+	    void setPublisherName (string n) { 
+			publisherName = n; 
+		}
+		
+	    string getCountry() const { 
+			return country; 
+		}
+		
+	    void setCountry(string c) { 
+			country = c; 
+		}
+};
