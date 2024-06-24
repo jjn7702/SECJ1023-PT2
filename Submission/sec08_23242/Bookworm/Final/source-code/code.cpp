c55daa98fac779dd51e90a2de28b63a16a0b87d6
Chuah Hui Wen <chwen0912@gmail.com>
2024-06-24 23:41:26 +0800

    Chuah Hui Wen

--- Submission/sec08_23242/Bookworm/Final/source-code/code.cpp
+++ Submission/sec08_23242/Bookworm/Final/source-code/code.cpp
@@ -34,3 +34,79 @@ class Publisher {
 			country = c; 
 		}
 };
+
+class Book {
+	protected:
+	    string bookCode;
+	    string bookTitle;
+	    string genre;
+	    int yearPublish;
+	    Publisher* publisher; // aggregation
+
+	public:
+	    Book() : publisher(NULL) {}
+	    
+	    Book(string bc, string bt, string g, int yp, Publisher* p) 
+	        : bookCode(bc), bookTitle(bt), genre(g), yearPublish(yp), publisher(p) {}
+	    
+		~Book() {}
+		
+	    string getBookCode() const { 
+			return bookCode; 
+		}
+		
+	    string getBookTitle() const { 
+			return bookTitle; 
+		}
+		
+	    string getGenre() const { 
+			return genre; 
+		}
+		
+	    int getYearPublish() const { 
+			return yearPublish; 
+		}
+		
+	    Publisher* getPublisher() const { 
+			return publisher; 
+		}
+	    
+		void setBookCode(string bc) { 
+			bookCode = bc; 
+		}
+		
+	    void setBookTitle(string bt) { 
+			bookTitle = bt; 
+		}
+		
+	    void setGenre(string g) { 
+			genre = g; 
+		}
+		
+	    void setYearPublish(int yp) { 
+			yearPublish = yp; 
+		}
+		
+	    void setPublisher(Publisher* p) { 
+			publisher = p; 
+		}
+	    
+	    virtual void display() const {  // use virtual to apply polymorphism
+	        cout << left << setw(8) << bookCode 
+	             << setw(25) << bookTitle 
+	             << setw(10) << genre 
+	             << setw(14) << yearPublish 
+	             << setw(31) << publisher->getPublisherName() + ", " + publisher->getCountry();
+	    }
+	    
+	    int getBookAge() const {
+	        time_t t = time(0);
+	        tm* now = localtime(&t);
+	        int currentYear = now->tm_year + 1900;
+	        return currentYear - yearPublish;
+	    }
+
+	    bool isClassic() const {
+	        return getBookAge() > 50;
+	    }
+};
