9c69144ec28fd01b1fd237ad16adac4f49c282dc
Foo Ming Kuang <150900178+mkuangdotcom@users.noreply.github.com>
2024-06-26 11:04:37 +0800

    Update Time.h

--- Submission/sec08_23242/DreamCatcher/Final/Source-code/Time.h
+++ Submission/sec08_23242/DreamCatcher/Final/Source-code/Time.h
@@ -16,7 +16,6 @@ class Time {
         int startTimes[30][2]; 
         int endTimes[30][2]; 
 
-
     public:
         Time(int _shour = 0, int _sminute = 0, int _ehour = 0, int _eminute = 0, int _day = 0) : shour(_shour), sminute(_sminute), ehour(_ehour), eminute(_eminute), day(_day) {
             for (int i = 0; i < 30; ++i) {
