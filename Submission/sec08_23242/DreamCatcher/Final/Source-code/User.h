12fb0fd0aeea61ae8ba7a17b38c0233c329064c0
Foo Ming Kuang <150900178+mkuangdotcom@users.noreply.github.com>
2024-06-26 11:05:21 +0800

    Update User.h

--- Submission/sec08_23242/DreamCatcher/Final/Source-code/User.h
+++ Submission/sec08_23242/DreamCatcher/Final/Source-code/User.h
@@ -6,20 +6,18 @@ using namespace std;
 #ifndef USER_H
 #define USER_H
 
-
 class User {
     protected:
         string username;
         string password;
 
     public:
-        User(string _username = "", string _password = ""): username(_username), password(_password) { } // Default constructor (with default values
+        User(string _username = "", string _password = ""): username(_username), password(_password) { } 
 
         void setUsername(string _username) { username = _username; }
         void setPassword(string _password) { password = _password; }
         string getUsername() const { return username; }
         string getPassword() const { return password; }
-
 };
 
 #endif
