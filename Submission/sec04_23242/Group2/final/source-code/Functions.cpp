b82694d6ff629c6bc6c11f8b7ec05da70759dc69
Lyson Ng De Sheng <147515831+Backfire301@users.noreply.github.com>
2024-06-26 22:18:08 +0800

    Add files via upload

---
+++ Submission/sec04_23242/Group2/final/source-code/Functions.cpp
@@ -0,0 +1,177 @@
+#include "Admin.h"
+#include "Time.h"
+#include "Reminder.h"
+#include "Event.h"
+#include <fstream>
+#include <string>
+using namespace std;
+
+void errorCheck(int &c, int start, int end)
+{
+    if (c < start || c > end)
+    {
+        cout << "value out of range, please try again\n";
+        cin >> c;
+        errorCheck(c, start, end);
+    }
+}
+
+void errorCheck(char &c)
+{
+    c = tolower(c);
+    if (c != 'y' && c != 'n')
+    {
+        cout << "Error, please reenter (Y/N) : ";
+        cin >> c;
+        c = tolower(c);
+        errorCheck(c);
+    }
+
+    else
+        return;
+}
+
+void errorCheck(string &s, int start, int end){
+    if(s == "0" || s == "00")
+        return;
+
+    int choice = stoi(s);
+
+    if (choice < start || choice > end)
+    {
+        cout << "value out of range, please try again\n";
+        cin >> s;
+        errorCheck(s, start, end);
+    }
+}
+
+void readFile(User user[])
+{
+    ifstream in("Usernames.txt");
+    int i = 0;
+    string temp;
+    if (in.fail())
+        throw "unable to find Usernames.txt file, exiting program\n\n";
+
+    while (!in.eof())
+    {
+        getline(in, temp, '\n');
+        user[i].setUsername(temp);
+        getline(in, temp, '\n');
+        user[i++].setPassword(temp);
+
+
+    }
+    in.close();
+}
+
+void writeFile(User user[])
+{
+    ofstream out("Usernames.txt");
+    for (int i = 0; i < 10; i++)
+    {
+        if (user[i].getUsername() != "")
+            out << user[i].getUsername() << endl << user[i].getPassword() << endl;
+
+    }
+    
+    out.close();
+}
+
+void writeSingleFile(User user)
+{
+    ofstream out;
+        if (user.getUsername() != "")
+        {
+            string temp = user.getUsername() + ".txt";
+            out.open(temp);
+            for (int j = 0; j < user.getNumEvent(); j++)
+            {
+                Time tempTime = user.getTime(j);
+                out << user.getOrganiser(j) << tempTime << user.getEventType(j) << endl;
+                if (user.getEventType(j) == 2)
+                {
+                    Time tempEndTime = user.getEndTime(j);
+                    out << tempEndTime;
+                }
+            }
+            out.close();
+        }
+}
+
+void readSingleFile(User &user)
+{
+    ifstream in(user.getUsername() + ".txt");
+    string temp;
+    Organiser tempOrg, tempOrg2;
+    Time tempTime;
+    while (!in.eof())
+    {
+        getline(in, temp);
+        if (temp == "")
+            break;
+        tempOrg.setEventName(temp.substr(21));
+        getline(in, temp);
+        tempOrg.setEventStatus(temp.substr(21, '\n') == "Done"? true: false);
+        getline(in, temp);
+        tempOrg.setEventDesc(temp.substr(21));
+        getline(in, temp);
+        tempTime.setDay(stoi(temp.substr(6, 2)));
+        tempTime.setMonth(stoi(temp.substr(9, 2)));
+        tempTime.setYear(stoi(temp.substr(12, 4)));
+        getline(in, temp);
+        tempTime.setHour(stoi(temp.substr(6, 2)));
+        tempTime.setMinute(stoi(temp.substr(9, 2)));
+        getline(in, temp);
+        tempTime.setStatus(temp.substr(8, '\n') == "Free"? false: true);
+        tempOrg.setStartTime(tempTime);
+        getline(in, temp);
+        int tempint = stoi(temp);
+        if (tempint == 2)
+        {
+            getline(in, temp);
+            tempTime.setDay(stoi(temp.substr(6, 2)));
+            tempTime.setMonth(stoi(temp.substr(9, 2)));
+            tempTime.setYear(stoi(temp.substr(12, 4)));
+            getline(in, temp);
+            tempTime.setHour(stoi(temp.substr(6, 2)));
+            tempTime.setMinute(stoi(temp.substr(9, 2)));
+            getline(in, temp);
+            tempTime.setStatus(temp.substr(8, '\n') == "Free"? false: true);
+        }
+
+        if (tempint == 0)
+        {
+            user.setOrganiser(new Organiser(tempOrg));
+        }
+
+        else if (tempint == 1)
+        {
+            user.setOrganiser(new Reminder(tempOrg));
+        }
+
+        else if (tempint == 2)
+        {
+            Event tempEvent(tempOrg);
+            tempEvent.setEndTime(tempTime);
+            user.setOrganiser(new Event(tempEvent));
+        }
+    }
+    in.close();
+}
+
+int getShortestTimeIndex(Time t[], int numEvent)
+{
+    Time temp = t[0];
+    int index = 0;
+    for (int i = 1; i < numEvent; i++)
+    {
+        if(t[i] < temp){
+            temp = t[i];
+            index = i;
+        }
+
+    }
+
+    return index;
+}
--- Submission/sec04_23242/Group2/final/source-code/main.cpp
+++ Submission/sec04_23242/Group2/final/source-code/main.cpp
@@ -1,186 +1,107 @@
-#include "User.h"
 #include "Admin.h"
-#include "Time.h"
-#include "Organiser.h"
 #include "Event.h"
 #include "Reminder.h"
 #include "Date.h"
+#include "Pages.cpp"
 #include "Schedule.h"
-#include <iostream>
-#include <string>
 using namespace std;
-/*
-int main(){
-    Time event;
-    event.readInput();
-    
-    Reminder r(&event, "Lunch", "somewhere", false);
-    r.notify();
-    cout << r;
 
-    system("pause");
-    return 0;
-
-}
-*/
-const int MAX_USERS = 100;
 
 int main ()
 {
-    int userCount = 0;
-    User users[MAX_USERS];
-    for (int i=0; i<MAX_USERS; i++){
-        cout << "----test for User.h----" << endl;
-        cout << "**Register New User**"<< endl;
-        users[i].newUser();
-        users[i].setID(++userCount);
-        string p;
-        bool r = false;
-        while (!r){
-            cout << "\nRe-check password: " ;
-            getline(cin, p);
-            r = users[i].checkPassword(p);
-        }
-        cout << " User succesfully registered." << endl;
-        break;
-    }    
-    system("pause");
-    system("cls");
-    
-    cout << "----test for Admin.h----" << endl;
-    Schedule s2(true), s3(true);
-    users[1] = User("user2", "pass2", s2, 2);
-    userCount++;
-    users[2] = User("user3", "pass3", s3, 3);
-    userCount++;
-    Admin a("admin", "adminpass");
-    cout << "All users:\n";
-    for (int i = 0; i < userCount; i++) {
-        cout << users[i].getID() << ". " << users[i].getUsername() << endl;
-    }
-    cout << "\n**Reset Password**" << endl;
-    a.resetPassword(users);
-    cout << "\n**Remove user**" << endl;
-    a.removeUser(users, userCount);
-    cout << "\nUpdated users:\n";
-    for (int i = 0; i < 100; i++) {
-        if (users[i].getID()!=0)
-            cout << users[i].getID() << ". " << users[i].getUsername() << endl;
-    }    
-    cout << "\n**Edit username**" << endl;
-    a.editUser(users);
-    
-    system("pause");
-    system("cls");
-
-    cout << "----test for Schedule.h----" << endl;
-    Schedule s(true);
-    s.printStatus();
-    s.setStatus(false);
-    s.printStatus();
-    system("pause");
-    system("cls");
-
-    cout << "\n\n----test for Date.h----" << endl;
-    Date d;
-    cout << "default date:\n";
-    d.printDate();
-    d.printStatus();
-
-    d.setDay(20);
-    d.setMonth(2);
-    d.setYear(2024);
-    d.setStatus(true);
-    cout << "\n\nedited date:\n";
-    d.printDate();
-    d.printStatus();
-    d.setStatus(false);
-    d.printStatus();
-    system("pause");
-    system("cls");
-
-    cout << "\n\n----test for Time.h----" << endl;
-    Time t, dt;
-    cout << "default Time:\n";
-    t.print();
-
-    cout << "\n\nedited Time:\n";
-    t.setYear(2025);
-    t.setMonth(6);
-    t.setDay(20);
-    t.setHour(10);
-    t.setMinute(20);
-    t.setStatus(false);
-    t.print();
-
-    cout << "\n\ntime difference:\n";
-    dt.getCurrentTime();
-    dt.setStatus(true);
-    dt.print();
-    dt = t - dt;
-    dt.diffPrint();
-
-    cout << "\n\ninput time from keyboard:\n";
-    t.readInput();
-    t.print();
-
-    system("pause");
-    system("cls");
-    
-    cout << "\n\n ----test for Organiser.h----" << endl;
-    Organiser o;
-    cout << "\n\ndefault organiser:\n" << o;
-    o.setEventDesc("description organiser");
-    o.setEventStatus(true);
-    o.setEventName("organiser name");
-    o.setStartTime(&t);
-    cout << "\n\nnew organiser: \n" << o;
-    system("pause");
-    system("cls");
-
-
-
-    cout << "\n\n----test for Event.h----" << endl;
-    Event e(new Time(), new Time(), "name", "description", true);
-    cout << "default event: \n\n" << e;
-    Time st(true, 2024, 7, 2, 13, 00);
-    Time et(true, 2024, 6, 2, 14, 00);
-    e.setStartTime(&st);
-    e.setEndTime(&et);
-    e.setEventName("name 2");
-    e.setEventDesc("decription 2");
-    e.setEventStatus(false);
-    cout << "\n\nnew event: \n" << e;
-    system("pause");
-    system("cls");
-
-    cout << "\n\n----test for Reminder.h:----\n";
-    Reminder r;
-    cout << "default reminder: \n";
-    r.notify();
-    r.setStartTime(&et);
-    r.setEventDesc("reminder desc");
-    r.setEventName("reminder name");
-    r.setEventStatus(true);
-    cout << "\n\nedited reminder: \n";
-    r.notify();
-    system("pause");
-    system("cls");
-
-    cout << "event array testing" << endl << endl;
-    Time starttime(false, 2024, 6, 20, 13, 10);
-    Time endtime(true, 2024, 7, 5, 11, 10);
-    Event event[3]{{&starttime, &endtime, "event name1", "event description 1", true}, {}, {}};
-    event[1].setEventStatus(1);
-    event[1].setEventName("event name 2");
-    event[1].setEventDesc("event desc 2");
-    event[1].setStartTime(&starttime);
-    event[1].setEndTime(&endtime);
-
-    for (int i = 0; i < 3; i++)
+    User users[10];
+    User user;
+    int choice;
+    try
     {
-        cout << "Event "<< i+1 << ".\n" << event[i] << endl;
+        readFile(users);
+    }
+    catch (const char* msg)
+    {
+        cout << msg;
+        system("pause");
+        return 0;
     }
 
-    system("pause");
+    bool nextPage = false;
+    startMenu:
+    do
+    {
+        int choice = startMenu();
+        switch (choice)
+        {
+            case 1:
+            nextPage = loginPage(users, user);
+            system("cls");
+            break;
+
+            case 2:
+            nextPage = newUser(users, user);
+            break;
+
+            case 0:
+            cout << "Program ended\n\n";
+            writeFile(users);
+            writeSingleFile(user);
+            system("pause");
+            return 0;
+        }
+    } while (!nextPage);
+
+    cout << "Welcome, " << user.getUsername() << "!\n";
+    readSingleFile(user);
+
+    Menu:
+    choice = menu(user);
+    string temp;
+    switch (choice)
+    {
+        case 1:
+        temp = menuTimeline(user);
+        if (temp == "0")
+            goto Menu;
+
+        if (temp == "00")
+            goto startMenu;
+
+        case 2:
+        goto editMenu;
+
+        case 0:
+        goto startMenu;
+    }
+
+    editMenu:
+    bool repeat = true;
+    do
+    {
+        string temp1 = editMenu();
+        string temp3;
+        if (temp1 == "0")
+            goto Menu;
+
+        if (temp1 == "00")
+            goto startMenu;
+
+        int temp1int = stoi(temp1);
+        switch (temp1int)
+        {
+            case 1:
+            user.setOrganiser(addEvent());
+            break;
+
+            case 2:
+            temp3 = removeEvent(user);
+            break;
+
+            case 3:
+            string temp2 = editEvent(user);
+            if (temp2 == "0")
+                goto editMenu;
+
+            if (temp2 == "00")
+                goto startMenu;
+            break;
+        }     
+    } while (repeat);
 }
