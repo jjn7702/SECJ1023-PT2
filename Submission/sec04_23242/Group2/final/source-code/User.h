d47cb3aa3dc9938bbba11d0573f114776a8fd112
Nik Balqis <148343355+nikbalqis@users.noreply.github.com>
2024-06-26 22:51:02 +0800

    Update User.h

--- Submission/sec04_23242/Group2/final/source-code/User.h
+++ Submission/sec04_23242/Group2/final/source-code/User.h
@@ -2,32 +2,49 @@
 #define USER_H
 
 #include "Schedule.h"
+#include "Organiser.h"
 #include <string>
+#include <vector>
+#define maxOrganiser 50
 using namespace std;
 
 class User {
 protected:
     string username;
     string password;
-    Schedule schedule;
+    vector <Organiser*> organiser;
+    int numEvent;
     int id;
 
 public:
     User();
-    User(string, string, Schedule, int);
+    User(string, string, int);
 
-    //accessor
-    bool checkPassword(string);
+    bool newUser(User[]);
+    bool passCheck(string, int);
+
+    //accessors
     string getUsername() const;
+    string getPassword() const;
     int getID() const;
-    Schedule getSchedule() const;
-    
+    Organiser getOrganiser(int ) const;
+    Organiser* getOrganiserPointer(int ) const;
+    string getOrganiserName(int ) const;
+    int getEventType(int ) const;
+    int getNumEvent()const;
+    Time getTime(int ) const;
+    Time getEndTime(int ) const;
+
+
     //mutators
-    void newUser();
     void setUsername(string);
     void setPassword(string);
-    void setSchedule(Schedule);
+    void setOrganiser(Organiser *);
+    void setOrganiser(Organiser *, int);
     void setID(int);
+
+    void removeEvent(int);
+    void arrange();
 };
 
 #endif
