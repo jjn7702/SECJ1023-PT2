b20b15e5b8bf04baa6b7e1a45aa81727d3a6b5c7
Lyson Ng De Sheng <147515831+Backfire301@users.noreply.github.com>
2024-06-26 22:16:11 +0800

    Update Time.h

--- Submission/sec04_23242/Group2/final/source-code/Time.h
+++ Submission/sec04_23242/Group2/final/source-code/Time.h
@@ -2,7 +2,10 @@
 #define TIME_H
 #include <string>
 #include <time.h>
+#include <iostream>
 #include "Date.h"
+using namespace std;
+
 
 class Time : public Date{
     protected:
@@ -11,7 +14,6 @@ class Time : public Date{
     public:
     Time();
     Time(bool, int, int ,int, int, int);
-    ~Time();
 
     //mutators
     void setHour(int);
@@ -22,13 +24,19 @@ class Time : public Date{
     int getMinute() const;
 
     void dateExtract(std::string);
+    void readInputTime();
     void readInput();
     void printTime();
     void print();
     void diffPrint();
     void getCurrentTime();
+    friend ostream& operator<< (ostream&, Time);
+    friend bool timeCompareForEvent(Time &, Time &);
+    friend bool timeCompareOneHour(Time &, Time &);
+
 
     Time operator-(const Time&);
+    bool operator<(const Time&);
 };
 
 #endif
