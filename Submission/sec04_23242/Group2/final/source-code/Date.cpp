175ef3026574cfa03d8a34754d89e82cfd061a5c
Lyson Ng De Sheng <147515831+Backfire301@users.noreply.github.com>
2024-06-26 22:15:14 +0800

    Update Date.cpp

--- Submission/sec04_23242/Group2/final/source-code/Date.cpp
+++ Submission/sec04_23242/Group2/final/source-code/Date.cpp
@@ -3,9 +3,8 @@
 #include <iomanip>
 using namespace std;
 
-Date :: Date(): Schedule(NULL), year(0), month(0), day(0) {}
-Date :: Date(bool s = false, int y = 0, int m = 0, int d = 0): year(y), month(m), day(d), Schedule(s){}
-Date :: ~Date() {}
+Date :: Date(): Schedule(true), year(0), month(0), day(0) {}
+Date :: Date(bool s = true, int y = 0, int m = 0, int d = 0): year(y), month(m), day(d), Schedule(s){}
 
 void Date :: setDay(int d) { day = d;}
 void Date :: setMonth(int m) { month = m;}
@@ -14,6 +13,11 @@ void Date :: setYear(int y) { year = y;}
 int Date :: getDay() const { return day;}
 int Date :: getMonth() const { return month;}
 int Date :: getYear() const { return year;}
+
+void Date :: readInput(){
+    cout << "Enter date(DD MM YYYY): ";
+    cin >> day >> month >> year;
+}
 
 void Date :: printDate(){
     cout << "Date: ";
@@ -25,5 +29,10 @@ void Date :: printDate(){
         if (month < 10)
             cout << "0";       
 
-        cout << month << "/" << year << endl;
+        cout << month << "/" << year << "\t";
+}
+
+ostream& operator<<(ostream& out, Date d){
+    d.printDate();
+    return out;
 }
