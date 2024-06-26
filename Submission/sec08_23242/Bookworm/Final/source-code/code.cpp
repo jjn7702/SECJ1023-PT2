a31c214b905562611157a9939abd4ea1ca7faeb7
Chen Wei Jay Nickolas <148372073+NickolasChen04@users.noreply.github.com>
2024-06-26 11:26:35 +0800

    Update code.cpp

--- Submission/sec08_23242/Bookworm/Final/source-code/code.cpp
+++ Submission/sec08_23242/Bookworm/Final/source-code/code.cpp
@@ -719,15 +719,15 @@ int main() {
 	                } while (choice8 < 1 || choice8 > 5 || choice9 == 1);
 	            } 
 
-		// if user input invalid genre choice at genre list page
-				else {
-					cin.clear(); // Clear the error flag
+		    // if user input invalid genre choice at genre list page
+		    else {
+			cin.clear(); // Clear the error flag
             		cin.ignore(INT_MAX, '\n'); // Discard invalid input
 	                cout << "Invalid genre choice\n";
 	                system("pause");
 	            }
-			 } while (genreChoice < 1 || genreChoice > 3);
-		}
+		} while (genreChoice < 1 || genreChoice > 3);
+	    }
 
 	    // if user choose option 3 in menu page
 	    else if (choice == 3) {
