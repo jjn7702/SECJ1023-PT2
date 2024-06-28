e8e70befb2d21700bc2dc46d65d491b1dc618e21
Alya Qistina binti Awaluddin <alyaqistina@graduate.utm.my>
2024-06-28 15:22:16 +0800

    Update Medication.hpp

--- Submission/sec04_23242/Group 9/Final/source-code/Medication.hpp
+++ Submission/sec04_23242/Group 9/Final/source-code/Medication.hpp
@@ -2,6 +2,14 @@
 #ifndef MEDICATION_H
 #define MEDICATION_H
 
+#include "MedType.hpp"
+#include "Frequency.hpp"
+#include "dailyFreq.hpp"
+#include "weeklyFreq.hpp"
+
+#include <iostream>
+using namespace std;
+
 class Medication {
     string medName, dosage;
     MedType medType;//composition
@@ -13,7 +21,7 @@ class Medication {
     //constructor
     Medication(){}
     //Medication(string n, string d): medName(n), dosage(d) {}
-    Medication(string n, string d, string f, string s, string c): medName(n), dosage(d), medType(f, c, s){}
+    Medication(string n, string d, string s, string c, string f): medName(n), dosage(d), medType(s,c,f){}
 
     //accessors
     string getMedName() {return medName;}
@@ -22,11 +30,12 @@ class Medication {
     //functions
     void input()
     {
-        cout << "\t\tEnter medication name: ";
+        cout << "Enter medication name: ";
         cin.ignore();
-    }
-
-    void freqInput() {
+        getline(cin, medName); 
+        cout << "Enter dosage(500mg, 5ml): ";
+        getline(cin, dosage);
+        medType.read();
         frequency.setFreq();
         dFreq.setdailyIntake();
         wFreq.setdayPerWeek();
@@ -44,17 +53,30 @@ class Medication {
         medType.printMedType();
     }
 
-    void display() {
-        cout << "\t\tMedicine Name: " << medName << endl;
-        cout << "\t\tMedicine Dosage: " << dosage << endl;
-        medType.printMedType();
+    void output(int num)
+    {
+        if(num==0){
+            cout << "No medication available.\n" << endl;
+        }else{
+            cout << left;
+            cout << setw(20) << "MEDICATION"<< setw(10) << "DOSAGE" << setw(10) << "FORM" << setw(10) << "SHAPE" << setw(10) << "COLOR" << endl;
+        }
     }
-
     void outputMed(){
-        cout << "\t\t" << setw(20) << medName << setw(10) << dosage << setw(10) << medType.getMedForm() << setw(10)<< medType.getMedShape() << setw(10) << medType.getMedColor() << "\n";
+        cout << setw(20) << medName << setw(10) << dosage << setw(10) << medType.getMedForm() << setw(10)<< medType.getMedShape() << setw(10) << medType.getMedColor() << "\n";
     }
 
-    //destructor 
+    void addtoFile(string filename) {
+        ofstream outfile(filename, ios::app);
+        if (outfile.is_open()) {
+            outfile << medName << " " << dosage << " " << medType.getMedForm() << " " << medType.getMedColor() << " " << medType.getMedShape() << endl;
+            outfile.close();
+        } else {
+            cout << "Error opening file for writing patient data." << endl;
+        }
+    }
+
+    //destructor
     ~Medication(){}
 };
 
